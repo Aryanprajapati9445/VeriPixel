8d18dd19f26c71c6ee12801d11bc2e53bc371689
Aryanprajapati9445 <aryanprajapati9582@gmail.com>
2026-02-20 10:48:03 +0530

    add favicon

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,23 @@ import io
 import joblib
 import numpy as np
 import cv2
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
 from features.preprocess import standardize_image
 from features.extract import extract_features
 from utils.validation import validate_image
 
 app = FastAPI()
+
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,8 +30,13 @@ app.add_middleware(
 
 model = joblib.load("model/model (2).pkl")
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Veripixel AI Image Detector API"}
+
 @app.post("/predict")
-async def predict(image: UploadFile = File(...)):
+@limiter.limit("5/minute")
+async def predict(request: Request, image: UploadFile = File(...)):
     file_bytes = await image.read()
 
     if not validate_image(file_bytes):
